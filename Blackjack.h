cd9000c6b9fa627b331b3874990767ff8fde5637
Tyler Toper <tyler.toper@ufl.edu>
2018-04-11 10:13:10 -0400

    Beginning of the test Blackjack class. May not even compile

---
+++ Blackjack.h
@@ -0,0 +1,15 @@
+//
+// Created by Tyler Toper on 4/11/2018.
+//
+
+class Blackjack: protected Game{
+private:
+   Player* players[4];
+
+public:
+};
+
+#ifndef GROUPCASINOPROJECT_BLACKJACK_H
+#define GROUPCASINOPROJECT_BLACKJACK_H
+
+#endif //GROUPCASINOPROJECT_BLACKJACK_H
